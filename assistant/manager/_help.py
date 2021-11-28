38ad034069d1a60759eb2c8fff47d642f52487d4
raviiranjan <90862124+raviiranjan@users.noreply.github.com>
2021-11-28 09:21:20 +0530

    Update _help.py

--- assistant/manager/_help.py
+++ assistant/manager/_help.py
@@ -1,7 +1,7 @@
-# Ultroid - UserBot
+# JOHN - UserBot
 # Copyright (C) 2021 TeamUltroid
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# This file is a part of < https://github.com/raviiranjan/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
@@ -14,7 +14,7 @@ START = """
 ✘  /help : Get This Message.
 ✘  /repo : Get Bot's Repo..
 
-🧑‍💻 Join **@TheUltroid**
+🧑‍💻 Join **@johnsupportuserbot**
 """
 
 ADMINTOOLS = """✘ **AdminTools** ✘
